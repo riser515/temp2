326092c8f00f229c5713ca5ac4d1edfbc0690e1e
Khushi <sparker.star001@gmail.com>
2021-08-20 15:56:58 +0530

    Rectified Unsubscription Functionality

--- unsubscribe.php
+++ unsubscribe.php
@@ -9,8 +9,10 @@ $DATABASE_NAME = 'phplogin';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
 if(@$_POST['submit']){
-    $stmt = $con->prepare('update accounts set unsubscribe = ? where id = ?');
-    $stmt->bind_param('ii', $_POST['unsubscribe'], $_SESSION['id']);
+    // $stmt = $con->prepare('update accounts set unsubscribe = ? where id = ?');
+    // $stmt->bind_param('ii', $_POST['unsubscribe'], $_SESSION['id']);
+    $stmt = $con->prepare('delete from accounts where id = ?');
+    $stmt->bind_param('i', $_SESSION['id']);
     $stmt->execute();
     // Store the result so we can check if the account exists in the database.
     $stmt->store_result();
