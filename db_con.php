980767f610a08cd6a25d207269d3903137716d89
Khushi <sparker.star001@gmail.com>
2021-08-29 00:34:48 +0530

    db details changed

--- db_con.php
+++ db_con.php
@@ -1,10 +1,15 @@
 <?php
 
 // Database connection info.
-$DATABASE_HOST = '127.0.0.1:3307';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+// $DATABASE_HOST = '127.0.0.1:3307';
+// $DATABASE_USER = 'root';
+// $DATABASE_PASS = '';
+// $DATABASE_NAME = 'phplogin';
+
+$DATABASE_HOST = 'us-cdbr-east-04.cleardb.com';
+$DATABASE_USER = 'ba38f742a2c2bd';
+$DATABASE_PASS = '04807876';
+$DATABASE_NAME = 'heroku_86a9ce3be59f4af';
 
 global $con;
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
