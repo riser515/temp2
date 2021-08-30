7a4fb3ba240a5e4a5e9e2ad9b208015ae096a176
Khushi <sparker.star001@gmail.com>
2021-08-30 11:39:15 +0530

    changed config var for heroku deployment

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
   //Get Heroku ClearDB connection information
+  // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  // $cleardb_server = $cleardb_url["host"];
+  // $cleardb_username = $cleardb_url["user"];
+  // $cleardb_password = $cleardb_url["pass"];
+  // $cleardb_db = substr($cleardb_url["path"],1);
+  // $active_group = 'default';
+  // $query_builder = TRUE;
   $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-  $cleardb_server = $cleardb_url["host"];
-  $cleardb_username = $cleardb_url["user"];
-  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
+  $cleardb_username = $cleardb_url["ba38f742a2c2bd"];
+  $cleardb_password = $cleardb_url["04807876"];
   $cleardb_db = substr($cleardb_url["path"],1);
   $active_group = 'default';
   $query_builder = TRUE;
--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
 include_once('db_con.php');
 
+global $con;
 if (mysqli_connect_errno()) {
 	// If there is an error with the connection, stop the script and display the error.
 	$msg = 'Failed to connect to MySQL: ' . mysqli_connect_error();
