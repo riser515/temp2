2d95645b4ef36bddba96c7529745395de9712ddc
Khushi <sparker.star001@gmail.com>
2021-08-30 12:42:19 +0530

    Added composer for sendgrid

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7a4fb3ba240a5e4a5e9e2ad9b208015ae096a176',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7a4fb3ba240a5e4a5e9e2ad9b208015ae096a176',
+            'dev_requirement' => false,
+        ),
+    ),
+);
