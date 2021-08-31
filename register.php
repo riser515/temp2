a463d63952a2b75a7abd794653eefc3f3c90bfb1
Khushi <sparker.star001@gmail.com>
2021-08-31 11:07:28 +0530

    Rectified validation for registration

--- register.php
+++ register.php
@@ -8,34 +8,34 @@ if (mysqli_connect_errno()) {
 }
 
 // Now we check if the data was submitted.
-if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+else if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
 	// Could not get the data that should have been sent.
 	$msg = 'Please complete the registration form!';
 }
 
 // Make sure the submitted registration values are not empty.
-if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+else if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
 	// One or more values are empty.
 	$msg = 'Please complete the registration form';
 }
 
 // Email validation
-if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 	$msg = 'Email is not valid!';
 }
 
 // Invalid characters validation
-if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
+else if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
     $msg = 'Username is not valid!';
 }
 
 // Character length check
-if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
-	$msg = 'Password must be between 5 and 20 characters long!';
+else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 6) {
+	$msg = 'Password must be 6 to 20 characters long!';
 }
 
 // If the account with that email exists.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) {
+else if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
 	$stmt->bind_param('s', $_POST['email']);
 	$stmt->execute();
